71b9790de36c3b9be1a6357cb0688ea16fb0f23a
William Rasakanya <63293037+WilliamRasakanya@users.noreply.github.com>
2024-06-23 15:52:03 +0200

    Add files via upload

--- automated_sh_generator_spix.py
+++ automated_sh_generator_spix.py
@@ -10,13 +10,40 @@
 # Generate a bash file will submit a script that uses python:
 #    to create folders, use astropy to get BMIN, BMAJ etc. values
 
-import os
+import os,sys
 
 cwd = os.getcwd()
+HOME = os.path.expanduser('~')
 
-#*******************************************
-# From Ian Heywood GitHub
-# https://github.com/IanHeywood/oxkat
+# ------------------------------------------------------------------------
+# From Ian Heywood oxkat github: https://github.com/IanHeywood/oxkat/
+#
+# Singularity settings
+#
+
+# Bind the symlinked location of the input image files.
+BIND = ''
+BINDPATH = '$PWD,'+CWD+','+BIND
+
+NODE_CONTAINER_PATH = [HOME+'/software/containers/']
+
+CASA = 'oxkat-0.41'
+
+# ------------------------------------------------------------------------
+
+# define how the method and machine to run the pipeline on
+
+def get_input(s):
+    try:
+        i = sys.argv.index(s)
+        return sys.argv[i+1]
+    except (ValueError, IndexError):
+        return None
+
+machine = get_input(1, "node")
+spi_method = get_input(2, 'manual')
+
+# ------------------------------------------------------------------------
 
 def make_executable(infile):
 
@@ -27,86 +54,109 @@ def make_executable(infile):
     os.chmod(infile, mode)    
 
     
-awk_arg = '{print $4}'    
+if machine == 'node' or None: 
 
-submit_file = 'submit_spi_creation_job.sh'
+    make_executable(submit_spi_z_absent)
 
-g = open(submit_file, 'w')
-g.write('#!/usr/bin/env bash \n')
-g.write(' \n#---------------------------------------- \n\n')
-g.write('# Use python and astropy to find the right beam size from the list of images \n')
-g.write("BeamExt=`sbatch " + cwd + "/slurm_beamext.sh | awk '{print $4}' `\n")
-g.write('\n    \n')
-g.write('# Use CASA to smooth the selected images \n')
-g.write("SMOOTH=`sbatch -d afterok:${BeamExt} " + cwd + "/slurm_smooth_regrid.sh | awk '{print $4}' ` \n")
-g.write('\n    \n')
-g.write('# Use BRATS to create a spectral index map \n')
-g.write("SPImap=`sbatch -d afterok:${SMOOTH} " + cwd + "/slurm_spi.sh | awk '{print $4}' `")
-g.close()
+    g = open(submit_spi_z_absent, 'w')
+    g.write('#!/usr/bin/bash' +'\n\n')
+    g.write('# Use python and astropy to find the right beam size from the list of images \n')
+    g.write("python " + cwd + "/beam_info_extraction.py \n\n")
+    g.write('# Use CASA to smooth the selected images \n')
+    g.write("singularity exec " + NODE_CONTAINER_PATH + CASA + " casa -c  " + cwd + "/smooth_regrid.py --log2term --nogui  \n\n")
+    if spi_method == 'manual' or None:
+        g.write('# Generate spectral index map using custom code \n')
+        g.write("python " + cwd + "/spec_index_z_absent.py \n")
+    elif spi_method == 'brats':
+        g.write('# Generate spectral index map using BRATS \n')
+        g.write('singularity exec /idia/software/containers/kern6.simg python3 spi_map.py ')
+    g.close()
 
-make_executable(submit_file) # From Ian Heywood GitHub
+elif machine == 'idia':
 
-submit_file_beamext = 'slurm_beamext.sh'
+    submit_file = 'submit_spi_creation_job.sh'
 
-g = open(submit_file_beamext, 'w')
-g.write('#!/usr/bin/bash' + '\n')
-g.write('\n')
-g.write('#SBATCH --job-name=BeamExt' + '\n')
-g.write('#SBATCH --time=12:00:00' + '\n')
-g.write('#SBATCH --partition=Main' + '\n')
-g.write('#SBATCH --ntasks=1' + '\n')
-g.write('#SBATCH --nodes=1' + '\n')
-g.write('#SBATCH --cpus-per-task=8' + '\n')
-g.write('#SBATCH --mem=64GB' + '\n')
-g.write('SECONDS=0 \n')
-g.write('echo "Submitting Slurm job -- Beam info extraction using astropy \n"')
-g.write('singularity exec /idia/software/containers/ASTRO-PY3.simg python ' + cwd + '/beam_info_extraction.py \n')
-g.write('echo "****ELAPSED "$SECONDS" BeamExt \n"')
-g.close()
+    g = open(submit_file, 'w')
+    g.write('#!/usr/bin/env bash \n')
+    g.write(' \n#---------------------------------------- \n\n')
+    g.write('# Use python and astropy to find the right beam size from the list of images \n')
+    g.write("BeamExt=`sbatch " + cwd + "/slurm_beamext.sh | awk '{print $4}' `\n")
+    g.write('\n    \n')
+    g.write('# Use CASA to smooth the selected images \n')
+    g.write("SMOOTH=`sbatch -d afterok:${BeamExt} " + cwd + "/slurm_smooth_regrid.sh | awk '{print $4}' ` \n")
+    g.write('\n    \n')
+    g.write('# Use BRATS to create a spectral index map \n')
+    g.write("SPImap=`sbatch -d afterok:${SMOOTH} " + cwd + "/slurm_spi.sh | awk '{print $4}' `")
+    g.close()
 
-make_executable(submit_file_beamext)
+    make_executable(submit_file) 
 
-submit_file_smooth = 'slurm_smooth_regrid.sh'
+    submit_file_beamext = 'slurm_beamext.sh'
 
-g = open(submit_file_smooth, 'w')
-g.write('#!/usr/bin/bash' + '\n')
-g.write('\n')
-g.write('#SBATCH --job-name=SMOOTH' + '\n')
-g.write('#SBATCH --time=12:00:00' + '\n')
-g.write('#SBATCH --partition=Main' + '\n')
-g.write('#SBATCH --ntasks=1' + '\n')
-g.write('#SBATCH --nodes=1' + '\n')
-g.write('#SBATCH --cpus-per-task=8' + '\n')
-g.write('#SBATCH --mem=64GB' + '\n')
-g.write('SECONDS=0 \n')
-g.write('echo "Submitting Slurm job -- Automated image smoothing and regridding \n"')
-g.write('singularity exec /idia/software/containers/casa-stable.img casa -c ' + cwd + '/smooth_regrid.py'+ ' --log2term --nogui \n')
-g.write('echo "****ELAPSED "$SECONDS" SMOOTH"')
-g.close()
+    g = open(submit_file_beamext, 'w')
+    g.write('#!/usr/bin/bash' + '\n')
+    g.write('\n')
+    g.write('#SBATCH --job-name=BeamExt' + '\n')
+    g.write('#SBATCH --time=12:00:00' + '\n')
+    g.write('#SBATCH --partition=Main' + '\n')
+    g.write('#SBATCH --ntasks=1' + '\n')
+    g.write('#SBATCH --nodes=1' + '\n')
+    g.write('#SBATCH --cpus-per-task=8' + '\n')
+    g.write('#SBATCH --mem=64GB' + '\n')
+    g.write('SECONDS=0 \n')
+    g.write('echo "Submitting Slurm job -- Beam info extraction using astropy \n"')
+    g.write('singularity exec /idia/software/containers/ASTRO-PY3.simg python ' + cwd + '/beam_info_extraction.py \n')
+    g.write('echo "****ELAPSED "$SECONDS" BeamExt \n"')
+    g.close()
 
-make_executable(submit_file_smooth) 
+    make_executable(submit_file_beamext)
 
-# From Jeremy Harwood GitHub
-# https://github.com/JeremyHarwood/bratswrapper
+    submit_file_smooth = 'slurm_smooth_regrid.sh'
 
-submit_file_spi = 'slurm_spi.sh'
+    g = open(submit_file_smooth, 'w')
+    g.write('#!/usr/bin/bash' + '\n')
+    g.write('\n')
+    g.write('#SBATCH --job-name=SMOOTH' + '\n')
+    g.write('#SBATCH --time=12:00:00' + '\n')
+    g.write('#SBATCH --partition=Main' + '\n')
+    g.write('#SBATCH --ntasks=1' + '\n')
+    g.write('#SBATCH --nodes=1' + '\n')
+    g.write('#SBATCH --cpus-per-task=8' + '\n')
+    g.write('#SBATCH --mem=64GB' + '\n')
+    g.write('SECONDS=0 \n')
+    g.write('echo "Submitting Slurm job -- Automated image smoothing and regridding \n"')
+    g.write('singularity exec /idia/software/containers/casa-stable.img casa -c ' + cwd + '/smooth_regrid.py'+ ' --log2term --nogui \n')
+    g.write('echo "****ELAPSED "$SECONDS" SMOOTH"')
+    g.close()
 
-g = open(submit_file_spi, 'w')
-g.write('#!/usr/bin/bash' +'\n')
-g.write('\n')
-g.write('#SBATCH --job-name=SPImap' + '\n')
-g.write('#SBATCH --time=12:00:00' + '\n')
-g.write('#SBATCH --partition=Main' + '\n')
-g.write('#SBATCH --ntasks=1' + '\n')
-g.write('#SBATCH --nodes=1' + '\n')
-g.write('#SBATCH --cpus-per-task=8' + '\n')
-g.write('#SBATCH --mem=128GB' + '\n')
-g.write('SECONDS=0 \n')
-g.write('echo "Submitting Slurm job -- Spectral index map creation using BRATS \n"')
-g.write('singularity exec /idia/software/containers/kern6.simg python3 spi_map.py \n')
-g.write('echo "****ELAPSED "$SECONDS" SPImap"')
-g.close()
+    make_executable(submit_file_smooth) 
 
-make_executable(submit_file_spi)
+    # From Jeremy Harwood GitHub
+    # https://github.com/JeremyHarwood/bratswrapper
+
+    submit_file_spi = 'slurm_spi.sh'
+
+    g = open(submit_file_spi, 'w')
+    g.write('#!/usr/bin/bash' +'\n')
+    g.write('\n')
+    g.write('#SBATCH --job-name=SPImap' + '\n')
+    g.write('#SBATCH --time=12:00:00' + '\n')
+    g.write('#SBATCH --partition=Main' + '\n')
+    g.write('#SBATCH --ntasks=1' + '\n')
+    g.write('#SBATCH --nodes=1' + '\n')
+    g.write('#SBATCH --cpus-per-task=8' + '\n')
+    g.write('#SBATCH --mem=128GB' + '\n')
+    g.write('SECONDS=0 \n')
+    if spi_method == 'manual':
+        g.write('# Generate spectral index map using custom code \n')
+        g.write("singularity exec /idia/software/containers/ASTRO-PY3.simg python " + cwd + "/spec_index_z_absent.py \n")
+    elif spi_method == 'brats':
+        g.write('echo "Submitting Slurm job -- Spectral index map creation using BRATS \n"')
+        g.write('singularity exec /idia/software/containers/kern6.simg python3 spi_map.py \n')
+    g.write('echo "****ELAPSED "$SECONDS" SPImap"')
+    g.close()
+
+    make_executable(submit_file_spi)
+
 
 
--- smooth_regrid.py
+++ smooth_regrid.py
@@ -6,10 +6,20 @@
 
 import os
 import glob
+from multiprocessing import Pool
 
 cwd = os.getcwd() # current working directory
 
 TXT = cwd + '/txt_files/'
+
+CASA_out = cwd + '/casa_output/'
+
+try:
+    os.mkdir(CASA_out)
+except:
+    pass
+
+DATA = cwd + '/data/'
 
 imagefiles = glob.glob('*.fits')  # Scan cwd for '.fits' files, import and put them in a list
 
@@ -27,41 +37,90 @@ g.close()
 
 # import fits to casa image format
 
-for image in imagefiles:    # first import fits to create images in casa format
+def import_im(image):
     importfits(fitsimage = image,
-            imagename = image[0:-5] + '.im',
+            imagename = CASA_out + image[0:-5] + '.im',
             overwrite=True)
-
-casaimages = glob.glob('*.im')
 
 # Image-smoothing
 
-for image in casaimages:
-    if image == exclude_im[0:-5] + '.im':
-        continue # smooth other images except the reference image
-
-    imsmooth(imagename = image,
-            kernel = 'g',
-            major = larger_major,
-            minor = larger_minor,
-            pa = larger_bpa,
-            targetres=True,
-            overwrite=True,
-            outfile = 'smoothed_' + image)
-    
-smoothedfiles = glob.glob('smoothed_*')
+def smooth(image):
+    if image[12:] not in exclude_im[0:-5] + '.im':
+        # smooth other images except the reference image
+        imsmooth(imagename = image,
+                major = larger_major,
+                minor = larger_minor,
+                pa = larger_bpa,
+                #targetres=True,
+                overwrite=True,
+                outfile = CASA_out + 'smoothed_' + image[12:])
 
 # image regridding
+REGRID = False # set to True if regridding is needed.
 
-for i in range(0, len(smoothedfiles)):   # now regrid the images || regrid others wrt a reference image
-    imregrid(imagename = smoothedfiles[i],
-            template = exclude_im[0:-5] + '.im',
-            output = smoothedfiles[i][0:-3] + '_regrid.im')
+def regrid(smoothedfiles):
+       # now regrid the images || regrid others wrt a reference image
+        imregrid(imagename = smoothedfiles,
+                template = exclude_im[0:-5] + '.im',
+                overwrite = True,
+                output = CASA_out + smoothedfiles[12:-3] + '_regrid.im')
 
-regridded = glob.glob('*regrid.im')
+# image export to fits
 
-# export casa im file to fits file
- 
-for image in regridded:
-    exportfits(image, cwd + '/data/' + image[0:-3] + '.fits', overwrite=True)
+def export(image):
+    exportfits(imagename = image, 
+        fitsimage = DATA + image[12:-3]+'.fits', overwrite=True)
+
+
+if __name__ == '__main__':
+    j = 40
+
+    images = sorted(glob.glob('*.fits'))
+
+    pool = Pool(processes=j)
+    pool.map(import_im,images)
+    pool.close()
+    pool.join()
+
+    casa_im = sorted(glob.glob('casa_output/'+'*.im'))
+
+    pool = Pool(processes=j)
+    pool.map(smooth,casa_im)
+    pool.close()
+    pool.join()
+
+    if REGRID == False:
+        smoothed_files = sorted(glob.glob('casa_output/' + 'smoothed*'))
+
+        pool = Pool(processes=j)
+        pool.map(export,smoothed_files)
+        pool.close()
+        pool.join()
+
+    elif REGRID == True:
+        smoothed_files = sorted(glob.glob('casa_output/' + 'smoothed*'))
+
+        pool = Pool(processes=j)
+        pool.map(regrid,smoothed_files)
+        pool.close()
+        pool.join()
+
+        regridded = sorted(glob.glob('casa_output/' + '*_regrid.im'))
+
+        pool = Pool(processes=j)
+        pool.map(export,regridded)
+        pool.close()
+        pool.join()
+
+
+
+
+
+
+
+
+
+# Cleaning up
+
+os.system('rm -r ' + CASA_out)
 

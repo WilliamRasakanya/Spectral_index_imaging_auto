69deed8fa1c389788ff7a892945230dae1b10950
William Rasakanya <63293037+WilliamRasakanya@users.noreply.github.com>
2024-12-10 18:02:41 +0200

    Update automated_sh_generator_spix.py

--- automated_sh_generator_spix.py
+++ automated_sh_generator_spix.py
@@ -56,111 +56,111 @@ def make_executable(infile):
     os.chmod(infile, mode)    
 
     
-    if machine == 'node' or None: 
+if machine == 'node' or None: 
 
-        submit_file = 'submit_spi_creation_job.sh'
-        
-        g = open(submit_file, 'w')
-        g.write('#!/usr/bin/bash' +'\n\n')
-        g.write('# Use python and astropy to find the right beam size from the list of images \n')
-        g.write("python " + cwd + "/beam_info_extraction.py \n\n")
-        g.write('# Use CASA to smooth the selected images \n')
-        g.write("singularity exec " + NODE_CONTAINER_PATH + CASA + " casa -c " + cwd + "/smooth_regrid.py --log2term --nogui  \n\n")
-        if spi_method == 'manual' or None:
-            g.write('# Generate spectral index map using custom code \n')
-            g.write("python " + cwd + "/spec_index_z_absent.py \n")
-        elif spi_method == 'brats':
-             g.write('# Generate spectral index map using BRATS \n')
-             g.write('singularity exec '+ NODE_CONTAINER_PATH + BRATS + ' python3 spi_map.py ')
-        g.close()
+    submit_file = 'submit_spi_creation_job.sh'
+    
+    g = open(submit_file, 'w')
+    g.write('#!/usr/bin/bash' +'\n\n')
+    g.write('# Use python and astropy to find the right beam size from the list of images \n')
+    g.write("python " + cwd + "/beam_info_extraction.py \n\n")
+    g.write('# Use CASA to smooth the selected images \n')
+    g.write("singularity exec " + NODE_CONTAINER_PATH + CASA + " casa -c " + cwd + "/smooth_regrid.py --log2term --nogui  \n\n")
+    if spi_method == 'manual' or None:
+        g.write('# Generate spectral index map using custom code \n')
+        g.write("python " + cwd + "/spec_index_z_absent.py \n")
+    elif spi_method == 'brats':
+         g.write('# Generate spectral index map using BRATS \n')
+         g.write('singularity exec '+ NODE_CONTAINER_PATH + BRATS + ' python3 spi_map.py ')
+    g.close()
 
-        make_executable(submit_file)
+    make_executable(submit_file)
 
-    elif machine == 'idia':
+elif machine == 'idia':
 
-        submit_file = 'submit_spi_creation_job.sh'
-    
-        g = open(submit_file, 'w')
-        g.write('#!/usr/bin/env bash \n')
-        g.write(' \n#---------------------------------------- \n\n')
-        g.write('# Use python and astropy to find the right beam size from the list of images \n')
-        g.write("BeamExt=`sbatch " + cwd + "/slurm_beamext.sh | awk '{print $4}' `\n")
-        g.write('\n    \n')
-        g.write('# Use CASA to smooth the selected images \n')
-        g.write("SMOOTH=`sbatch -d afterok:${BeamExt} " + cwd + "/slurm_smooth_regrid.sh | awk '{print $4}' ` \n")
-        g.write('\n    \n')
-        g.write('# Use BRATS to create a spectral index map \n')
-        g.write("SPImap=`sbatch -d afterok:${SMOOTH} " + cwd + "/slurm_spi.sh | awk '{print $4}' `")
-        g.close()
+    submit_file = 'submit_spi_creation_job.sh'
 
-        make_executable(submit_file) 
-    
-        submit_file_beamext = 'slurm_beamext.sh'
-    
-        g = open(submit_file_beamext, 'w')
-        g.write('#!/usr/bin/bash' + '\n')
-        g.write('\n')
-        g.write('#SBATCH --job-name=BeamExt' + '\n')
-        g.write('#SBATCH --time=12:00:00' + '\n')
-        g.write('#SBATCH --partition=Main' + '\n')
-        g.write('#SBATCH --ntasks=1' + '\n')
-        g.write('#SBATCH --nodes=1' + '\n')
-        g.write('#SBATCH --cpus-per-task=8' + '\n')
-        g.write('#SBATCH --mem=64GB' + '\n')
-        g.write('SECONDS=0 \n')
-        g.write('echo "Submitting Slurm job -- Beam info extraction using astropy \n"')
-        g.write('singularity exec /idia/software/containers/ASTRO-PY3.simg python ' + cwd + '/beam_info_extraction.py \n')
-        g.write('echo "****ELAPSED "$SECONDS" BeamExt \n"')
-        g.close()
-    
-        make_executable(submit_file_beamext)
-    
-        submit_file_smooth = 'slurm_smooth_regrid.sh'
-    
-        g = open(submit_file_smooth, 'w')
-        g.write('#!/usr/bin/bash' + '\n')
-        g.write('\n')
-        g.write('#SBATCH --job-name=SMOOTH' + '\n')
-        g.write('#SBATCH --time=12:00:00' + '\n')
-        g.write('#SBATCH --partition=Main' + '\n')
-        g.write('#SBATCH --ntasks=1' + '\n')
-        g.write('#SBATCH --nodes=1' + '\n')
-        g.write('#SBATCH --cpus-per-task=8' + '\n')
-        g.write('#SBATCH --mem=64GB' + '\n')
-        g.write('SECONDS=0 \n')
-        g.write('echo "Submitting Slurm job -- Automated image smoothing and regridding \n"')
-        g.write('singularity exec /idia/software/containers/casa-stable.img casa -c ' + cwd + '/smooth_regrid.py'+ ' --log2term --nogui \n')
-        g.write('echo "****ELAPSED "$SECONDS" SMOOTH"')
-        g.close()
-    
-        make_executable(submit_file_smooth) 
-    
-        # From Jeremy Harwood GitHub
-        # https://github.com/JeremyHarwood/bratswrapper
-    
-        submit_file_spi = 'slurm_spi.sh'
-    
-        g = open(submit_file_spi, 'w')
-        g.write('#!/usr/bin/bash' +'\n')
-        g.write('\n')
-        g.write('#SBATCH --job-name=SPImap' + '\n')
-        g.write('#SBATCH --time=12:00:00' + '\n')
-        g.write('#SBATCH --partition=Main' + '\n')
-        g.write('#SBATCH --ntasks=1' + '\n')
-        g.write('#SBATCH --nodes=1' + '\n')
-        g.write('#SBATCH --cpus-per-task=8' + '\n')
-        g.write('#SBATCH --mem=128GB' + '\n')
-        g.write('SECONDS=0 \n')
-        if spi_method == 'manual':
-            g.write('# Generate spectral index map using custom code \n')
-            g.write("singularity exec /idia/software/containers/ASTRO-PY3.simg python " + cwd + "/spec_index_z_absent.py \n")
-        elif spi_method == 'brats':
-            g.write('echo "Submitting Slurm job -- Spectral index map creation using BRATS \n"')
-            g.write('singularity exec /idia/software/containers/kern6.simg python3 spi_map.py \n')
-        g.write('echo "****ELAPSED "$SECONDS" SPImap"')
-        g.close()
-    
-        make_executable(submit_file_spi)
+    g = open(submit_file, 'w')
+    g.write('#!/usr/bin/env bash \n')
+    g.write(' \n#---------------------------------------- \n\n')
+    g.write('# Use python and astropy to find the right beam size from the list of images \n')
+    g.write("BeamExt=`sbatch " + cwd + "/slurm_beamext.sh | awk '{print $4}' `\n")
+    g.write('\n    \n')
+    g.write('# Use CASA to smooth the selected images \n')
+    g.write("SMOOTH=`sbatch -d afterok:${BeamExt} " + cwd + "/slurm_smooth_regrid.sh | awk '{print $4}' ` \n")
+    g.write('\n    \n')
+    g.write('# Use BRATS to create a spectral index map \n')
+    g.write("SPImap=`sbatch -d afterok:${SMOOTH} " + cwd + "/slurm_spi.sh | awk '{print $4}' `")
+    g.close()
+
+    make_executable(submit_file) 
+
+    submit_file_beamext = 'slurm_beamext.sh'
+
+    g = open(submit_file_beamext, 'w')
+    g.write('#!/usr/bin/bash' + '\n')
+    g.write('\n')
+    g.write('#SBATCH --job-name=BeamExt' + '\n')
+    g.write('#SBATCH --time=12:00:00' + '\n')
+    g.write('#SBATCH --partition=Main' + '\n')
+    g.write('#SBATCH --ntasks=1' + '\n')
+    g.write('#SBATCH --nodes=1' + '\n')
+    g.write('#SBATCH --cpus-per-task=8' + '\n')
+    g.write('#SBATCH --mem=64GB' + '\n')
+    g.write('SECONDS=0 \n')
+    g.write('echo "Submitting Slurm job -- Beam info extraction using astropy \n"')
+    g.write('singularity exec /idia/software/containers/ASTRO-PY3.simg python ' + cwd + '/beam_info_extraction.py \n')
+    g.write('echo "****ELAPSED "$SECONDS" BeamExt \n"')
+    g.close()
+
+    make_executable(submit_file_beamext)
+
+    submit_file_smooth = 'slurm_smooth_regrid.sh'
+
+    g = open(submit_file_smooth, 'w')
+    g.write('#!/usr/bin/bash' + '\n')
+    g.write('\n')
+    g.write('#SBATCH --job-name=SMOOTH' + '\n')
+    g.write('#SBATCH --time=12:00:00' + '\n')
+    g.write('#SBATCH --partition=Main' + '\n')
+    g.write('#SBATCH --ntasks=1' + '\n')
+    g.write('#SBATCH --nodes=1' + '\n')
+    g.write('#SBATCH --cpus-per-task=8' + '\n')
+    g.write('#SBATCH --mem=64GB' + '\n')
+    g.write('SECONDS=0 \n')
+    g.write('echo "Submitting Slurm job -- Automated image smoothing and regridding \n"')
+    g.write('singularity exec /idia/software/containers/casa-stable.img casa -c ' + cwd + '/smooth_regrid.py'+ ' --log2term --nogui \n')
+    g.write('echo "****ELAPSED "$SECONDS" SMOOTH"')
+    g.close()
+
+    make_executable(submit_file_smooth) 
+
+    # From Jeremy Harwood GitHub
+    # https://github.com/JeremyHarwood/bratswrapper
+
+    submit_file_spi = 'slurm_spi.sh'
+
+    g = open(submit_file_spi, 'w')
+    g.write('#!/usr/bin/bash' +'\n')
+    g.write('\n')
+    g.write('#SBATCH --job-name=SPImap' + '\n')
+    g.write('#SBATCH --time=12:00:00' + '\n')
+    g.write('#SBATCH --partition=Main' + '\n')
+    g.write('#SBATCH --ntasks=1' + '\n')
+    g.write('#SBATCH --nodes=1' + '\n')
+    g.write('#SBATCH --cpus-per-task=8' + '\n')
+    g.write('#SBATCH --mem=128GB' + '\n')
+    g.write('SECONDS=0 \n')
+    if spi_method == 'manual':
+        g.write('# Generate spectral index map using custom code \n')
+        g.write("singularity exec /idia/software/containers/ASTRO-PY3.simg python " + cwd + "/spec_index_z_absent.py \n")
+    elif spi_method == 'brats':
+        g.write('echo "Submitting Slurm job -- Spectral index map creation using BRATS \n"')
+        g.write('singularity exec /idia/software/containers/kern6.simg python3 spi_map.py \n')
+    g.write('echo "****ELAPSED "$SECONDS" SPImap"')
+    g.close()
+
+    make_executable(submit_file_spi)
 
 
 
